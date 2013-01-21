7e3535ba5c9f777a90493158b83a4232e5dbe8bb
Cameron Pope <cameron@theaboutbox.com>
2013-01-21 06:22:00 -0700

    Empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GsaProductSearch::Application.config.secret_token = '662c3727238887e8db09a03b2359e0599ab10a29bffda77f2c646ce4779146a33467d4286cbacd5cf4a145b29b934258f670ed1ec329b29593b05253e9fbaed5'
